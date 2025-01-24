4a8317a4c84b51c7520880a280b9281dc63768fe
S.M Shihab Adnan Saad <ssaad222135@bscse.uiu.ac.bd>
2025-01-24 06:04:16 +0600

    admin panel

---
+++ admin.js
@@ -0,0 +1,9 @@
+
+const reviewBtn = document.querySelector('.review-btn');
+reviewBtn.addEventListener('click', () => {
+  alert('Opening Reviews Section...');
+});
+const chatbotBtn = document.querySelector('.chatbot-btn');
+chatbotBtn.addEventListener('click', () => {
+  alert('Opening Chatbot...');
+});
